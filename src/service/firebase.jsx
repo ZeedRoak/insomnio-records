aff088c30fe13f539d1b98367978660a32c336cc
Maximiliano <maxiwulf32@outlook.com>
2026-07-03 03:09:49 -0300

    Fix firebase

--- src/service/firebase.jsx
+++ src/service/firebase.jsx
@@ -3,14 +3,14 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  authDomain: "disco-insomnio.firebaseapp.com",
+  projectId: "disco-insomnio",
+  storageBucket: "disco-insomnio.firebasestorage.app",
+  messagingSenderId: "329678324326",
+  appId: "1:329678324326:web:bc9d4363e78042079023a7",
+  measurementId: "G-1X273JTD0R"
+ 
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
